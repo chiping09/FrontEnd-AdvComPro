22aef2b64dc0c55796986ff29c1dee2c7040c4c5
chiping09 <chip__ing@hotmail.com>
2024-09-11 09:26:03 +0700

    Week9

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -6,7 +6,7 @@ const app = express();
 
 const base_url = "http://localhost:3000";
 
-app.set("views", path.join(__dirname, "/views"));
+app.set("views", path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
